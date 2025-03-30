fad214c8b68e2143fa829061f5493a6460659330
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-30 11:58:21 +0530

    Modified connection string

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,7 +4,7 @@ export const connectToDB = async()=>{
     mongoose.set('strictQuery' , true);
     if(isConnected) return console.log("Already connected to DataBase");
     try {
-        await mongoose.connect(process.env.MONGODB_URI!)
+        await mongoose.connect("mongodb+srv://navinofficial2005:TomCruise@cluster0.ngxz7tp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         isConnected =true;
         console.log("connected to DB")
     } catch (error) {
